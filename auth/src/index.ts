9ad7dc9cb6b82be8f0963b6ed094a69bd330ca11
Devesh Avutu <devesh.avutu@tesco.com>
2020-07-17 22:01:26 +0530

    settingup kubernetes setup for auth service

---
+++ auth/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+const app = express();
+app.use(json());
+
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('hi there');
+});
+
+app.listen(4000, () => {
+  console.log("auth service listening on port 4000!!!");
+});
