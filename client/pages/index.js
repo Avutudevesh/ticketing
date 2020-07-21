1b34e0f3ad590672338617aa63b235eb2b103730
Devesh Avutu <devesh.avutu@tesco.com>
2020-07-21 20:43:27 +0530

    implemented signup page on client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,11 @@
-export default () => {
-  return <h1>Banana</h1>;
+import axios from 'axios';
+const LandingPage = () => {
+  return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get('/api/users/currentuser');
+  return response.data;
 }
+
+export default LandingPage;
