9aa93ef7c5779baa17d55af9e6ffe683218b38a1
Devesh Avutu <devesh.avutu@tesco.com>
2020-07-27 20:06:19 +0530

    implemented new orders route handler

---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,61 @@
+import mongoose from 'mongoose';
+import { Order } from './order';
+import { OrderStatus } from '@daticketing/common';
+
+interface TicketAttrs {
+  title: string;
+  price: number;
+}
+
+interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+  isReserved(): Promise<boolean>;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc
+}
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  }
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id,
+        delete ret._id
+    }
+  }
+});
+
+schema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+}
+
+schema.methods.isReserved = async function () {
+  const exiastingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.CREATED,
+        OrderStatus.COMPLETED,
+        OrderStatus.AWAITING_PAYMENT
+      ]
+    }
+  });
+
+  return !!exiastingOrder;
+
+}
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', schema);
+
+export { Ticket, TicketDoc };
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@daticketing/common';
+import { requireAuth, validateRequest, BadRequestError, OrderStatus } from '@daticketing/common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
-
+const EXPIRATION_WINDOW_SECONDS = 30;
 router.post('/api/orders',
   requireAuth,
   [
@@ -14,7 +16,26 @@ router.post('/api/orders',
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send({});
+    const { ticketId } = req.body;
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new BadRequestError('Ticket trying to book is not found');
+    }
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError('Ticket already reserved');
+    }
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.CREATED,
+      expiresAt: expiration,
+      ticket
+    });
+    await order.save();
+    res.status(201).send(order);
   });
 
 export { router };
