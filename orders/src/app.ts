0219b4183ebf73982d54d1adb0876bc40b565b0c
Devesh Avutu <devesh.avutu@tesco.com>
2020-07-27 13:26:06 +0530

    skaffolding orders routes

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,6 +3,10 @@ import cookieSession from 'cookie-session';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import { errorHandler, NotFoundError, currentUser } from '@daticketing/common';
+import { router as indexRouter } from './routes/index';
+import { router as deleteRouter } from './routes/delete';
+import { router as newRouter } from './routes/new';
+import { router as showRouter } from './routes/show';
 
 
 const app = express();
@@ -13,6 +17,10 @@ app.use(cookieSession({
   secure: process.env.NODE_ENV !== 'test'
 }));
 app.use(currentUser);
+app.use(indexRouter);
+app.use(deleteRouter);
+app.use(newRouter);
+app.use(showRouter);
 
 
 app.all('*', async () => {
---
+++ orders/src/routes/new.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@daticketing/common';
+import { body } from 'express-validator';
+
+const router = express.Router();
+
+router.post('/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .withMessage('Ticket id must be provided')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  });
+
+export { router };
